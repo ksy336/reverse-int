237761d72ed9b1406570b66ad436a443f55030b2
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-03 22:21:02 +0400

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if(typeof n !== 'number' && isNaN(n)) {return false};
+        let numpositive = Math.abs(n);
+        let stringFromNum = numpositive.toString().split('').reverse().join('');
+        return (+stringFromNum);
+
+
+
+
 }
